f312754a93b0dd925fef416be6ad467907676b98
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-04 21:24:17 +0000

    automatically backed up by learn

--- db/migrate/02_update_cook_time_in_recipes.rb
+++ db/migrate/02_update_cook_time_in_recipes.rb
@@ -1,5 +1,9 @@
 class UpdateCookTimeInRecipes < ActiveRecord::Migration
   def up 
-    change_column :recipes, :
+    change_column :recipes, :cook_time, t.integer 
+  end 
+  
+  def down
+    change
   end 
 end 
